e26357cd80afc56c3b36d995f23a682dc2f7e855
senthil25393 <senthilshyam25@gmail.com>
2022-05-27 23:31:08 +0530

    Commit using eclipse

---
+++ add/src/org/add/Add.java
@@ -0,0 +1,27 @@
+package org.add;
+
+public class Add {
+	
+	private int aDd() {
+		// TODO Auto-generated method stub
+		int a=10,b=20,c;
+		c=a+b;
+		System.out.println(c);
+		return c;
+		
+		
+
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Add d = new Add();
+		int avg = d.aDd();
+		System.out.println("Average is :"+avg/5);
+		
+	
+		
+	}
+
+}
